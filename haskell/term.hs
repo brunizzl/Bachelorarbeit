3f1c5bebeb6eaf608e8c7423cd30a277a637bfa6
Bruno <46483121+brunizzl@users.noreply.github.com>
2021-04-17 00:34:44 +0200

    queeeeellen

--- haskell/term.hs
+++ haskell/term.hs
@@ -3,29 +3,29 @@ import qualified Data.Map as Map
 import qualified Control.Monad as Mon
 
 
-data Term f s = Const s | Call f [Term f s] deriving (Eq, Show)
+data Term f c = Const c | Call f [Term f c] deriving (Eq, Show)
 
 --extends a Set of function symbols or constant symbols to also include 
 --  matching variable symbols
 data PatVar a = Pat Int | Lit a deriving (Show)
 
-eval :: (f -> [s] -> s) -> (Term f s) -> s
-eval v (Const t)   = t
-eval v (Call f ts) = v f (map (eval v) ts)
+eval :: (f -> [y] -> y) -> (c -> y) -> (Term f c) -> y
+eval u v (Const t)   = v t
+eval u v (Call f ts) = u f (map (eval u v) ts)
 
-match :: (Eq f, Eq s) => (Term (PatVar f) (PatVar s)) -> (Term f s) -> (Maybe (Map.Map Int (Term f s)))
-match (Call (Lit f) ts) (Call g rs) 
+match :: (Eq f, Eq c) => (Term f (PatVar c)) -> (Term f c) -> (Maybe (Map.Map Int (Term f c)))
+match (Call f ts) (Call g rs) 
     | f /= g                 = Nothing
     | length ts /= length rs = Nothing
     | otherwise              = undefined
-match (Call (Pat v) ts) (Call g rs) = undefined
 
 
 
 data RealOps = Sum | Product | Negate | Invert deriving (Eq, Show)
 
-evalReal :: (Fractional s) => RealOps -> [s] -> s
+evalReal :: (Fractional c) => RealOps -> [c] -> c
 evalReal Sum     cs = sum     cs
 evalReal Product cs = product cs
 evalReal Negate [c] = -c
 evalReal Invert [c] = 1 / c
+
